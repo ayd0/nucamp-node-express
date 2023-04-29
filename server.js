f0953e33a92df32ffc9f8e69161e11f7a5af82b8
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-04-28 19:52:37 -0700

    part 1 REST operations

--- server.js
+++ server.js
@@ -1,18 +1,69 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
 
 const app = express(); // returns express server application
 
-app.use(morgan('dev')); // middleware function: Express term
-app.use(express.static(__dirname + '/public')); // serves static files from public folder
+app.use(morgan("dev")); // middleware function: Express term
+app.use(express.json());
 
-app.use((req, res) => { 
+app.all("/campsites", (req, res, next) => {
+    // catches all HTTP verbs to path
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express server</h1></body></html>');
+    res.setHeader("Content-Type", "text/plain");
+    next(); // passes control of app routing to next relavent routing method
+});
+
+app.get("/campsites", (req, res) => {
+    res.end("Will send all the campsites to you");
+});
+
+app.post("/campsites", (req, res) => {
+    res.end(
+        `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+    );
+});
+
+app.put("/campsites", (req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /campsites");
+});
+
+app.delete("/campsites", (req, res) => {
+    res.end("Deleting all campsites");
+});
+
+app.get("/campsites/:campsiteId", (req, res) => {
+    res.end(
+        `Will send details of the campsite: ${req.params.campsiteId} to you`
+    );
+});
+
+app.post("/campsites/:campsiteId", (req, res) => {
+    res.statusCode = 403;
+    res.end(
+        `POST operation not supported on /campsites/${req.params.campsiteId}`
+    );
+});
+
+app.put("/campsites/:campsiteId", (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+app.use(express.static(__dirname + "/public")); // serves static files from public folder
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<html><body><h1>This is an Express server</h1></body></html>");
 });
 
 app.listen(port, hostname, () => {
